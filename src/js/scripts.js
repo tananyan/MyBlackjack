6e4cfd121f77f6af203dfa73cff9e6753e2603e7
Boris <Boris242008@ya.ru>
2022-04-14 01:20:38 +0300

    c

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,52 +1,72 @@
 // document.addEventListener("DOMContentLoaded", () => {});
-// document.addEventListener("DOMContentLoaded", function () {
+"use strict";
 
-const suits = ["♠", "♣", "♥", "♦"];
-const values = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K",
-];
+document.addEventListener("DOMContentLoaded", function () {
+  const suits = ["♠", "♣", "♥", "♦"];
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
 
-function buildCards(suitsArr, valuesArr) {
-  let cardsArr = [];
-  suitsArr.forEach((itemSuit) => {
-    valuesArr.forEach((itemValue) => {
-      let card = {};
-      card.suit = itemSuit;
-      card.value = itemValue;
-      cardsArr.push(card);
+  function buildCards(suitsArr, valuesArr) {
+    let cardsArr = [];
+    suitsArr.forEach((itemSuit) => {
+      valuesArr.forEach((itemValue) => {
+        let card = {};
+        card.suit = itemSuit;
+        card.value = itemValue;
+        cardsArr.push(card);
+      });
     });
+    return cardsArr;
+  }
+  let cards = buildCards(suits, values);
+
+  const cardONE = document.querySelector("#cardOne");
+  const cardTWO = document.querySelector("#cardTwo");
+  const cardTop = document.querySelectorAll(".game__card-top");
+  const cardMid = document.querySelectorAll(".game__card-mid");
+  const cardBot = document.querySelectorAll(".game__card-bot");
+  const startBtn = document.querySelector("#start");
+  const score = document.querySelector(".game__score");
+
+  let num = 0;
+  let cardNUM = [cardONE, cardTWO];
+
+  function getCard(num) {
+    let n = Number(Math.floor(Math.random() * cards.length));
+    if (cards[n].suit == "♥" || cards[n].suit == "♦") {
+      cardNUM[num].style.color = "#ff3300";
+    } else {
+      cardNUM[num].style.color = "#000000";
+    }
+
+    cardTop[num].innerHTML = cards[n].value;
+    cardMid[num].innerHTML = cards[n].suit;
+    cardBot[num].innerHTML = cards[n].value;
+
+    score.innerHTML += cards[n].value;
+  }
+
+  startBtn.addEventListener("click", () => {
+    getCard(num);
+    num++;
   });
-  return cardsArr;
-}
+  // const lang = document.querySelector(".footer__lang");
+  // lang.addEventListener("click", () => {
+  //   langSelect.style.display = "block";
+  // });
 
-let cards = buildCards(suits, values);
-
-rndCard.onclick = function () {
-  cardOne.innerHTML = Number(Math.floor(Math.random() * 10));
-};
-
-rndCard.addEventListener("click", () => {
-  // cardTwo.innerHTML = Number(Math.floor(Math.random() * 10));
-  let n = Number(Math.floor(Math.random() * cards.length));
-  cardTwo.innerHTML = cards[n].value + cards[n].suit;
+  //
 });
-
-// const lang = document.querySelector(".footer__lang");
-// lang.addEventListener("click", () => {
-//   langSelect.style.display = "block";
-// });
-
-//
-// });
