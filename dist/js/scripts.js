2a80a622998155ca046c5a5596e33564859e88b2
Boris <Boris242008@ya.ru>
2022-04-13 20:46:18 +0300

    123

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,32 +1,52 @@
 // document.addEventListener("DOMContentLoaded", () => {});
-document.addEventListener("DOMContentLoaded", function () {
-  // lang
-  const lang = document.querySelector(".footer__lang"),
-    langSelect = document.querySelector(".footer__lang-select");
+// document.addEventListener("DOMContentLoaded", function () {
 
-  lang.addEventListener("click", () => {
-    langSelect.style.display = "block";
+const suits = ["♠", "♣", "♥", "♦"];
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
+
+function buildCards(suitsArr, valuesArr) {
+  let cardsArr = [];
+  suitsArr.forEach((itemSuit) => {
+    valuesArr.forEach((itemValue) => {
+      let card = {};
+      card.suit = itemSuit;
+      card.value = itemValue;
+      cardsArr.push(card);
+    });
   });
-  langSelect.addEventListener("click", () => {
-    langSelect.style.display = "none";
-  });
+  return cardsArr;
+}
 
-  // scroll
-  const scrollUp = document.querySelector(".scroll-up");
+let cards = buildCards(suits, values);
 
-  function myFunction() {
-    if (
-      document.body.scrollTop > 870 ||
-      document.documentElement.scrollTop > 870
-    ) {
-      scrollUp.style.display = "block";
-    } else {
-      scrollUp.style.display = "none";
-    }
-  }
-  window.onscroll = function () {
-    myFunction();
-  };
+rndCard.onclick = function () {
+  cardOne.innerHTML = Number(Math.floor(Math.random() * 10));
+};
 
-  //
+rndCard.addEventListener("click", () => {
+  // cardTwo.innerHTML = Number(Math.floor(Math.random() * 10));
+  let n = Number(Math.floor(Math.random() * cards.length));
+  cardTwo.innerHTML = cards[n].value + cards[n].suit;
 });
+
+// const lang = document.querySelector(".footer__lang");
+// lang.addEventListener("click", () => {
+//   langSelect.style.display = "block";
+// });
+
+//
+// });
