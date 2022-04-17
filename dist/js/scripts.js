effe791bc6dec1622aa6965dd34ccbd289e07fb9
Boris <Boris242008@ya.ru>
2022-04-17 03:01:17 +0300

    работает

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -38,9 +38,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const startGame = document.querySelector("#newGame");
   const menuGame = document.querySelector(".game__menu");
   const boxGame = document.querySelector(".game__start");
+  // Номер карты
+  let numD = 0;
+  let sumD = 0;
   startGame.addEventListener("click", () => {
     menuGame.style.display = "none";
     boxGame.style.display = "block";
+    getCardDealer(numD);
   });
 
   // Игра
@@ -66,7 +70,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const stopBtn = document.querySelector("#EnoughBtn");
   const score = document.querySelector(".game__score");
 
-  let num = 0;
+  // Номер карты игрока
+  let num = 5;
+  let sum = 0;
   let cardNUM = [
     cardONEd,
     cardTWOd,
@@ -80,7 +86,11 @@ document.addEventListener("DOMContentLoaded", function () {
     cardFIVE,
   ];
 
-  function getCard(num) {
+  function getCardDealer(numD) {
+    getCard(numD, false);
+  }
+
+  function getCard(num, flag) {
     let n = Number(Math.floor(Math.random() * cards.length));
     if (cards[n].suit == "♥" || cards[n].suit == "♦") {
       cardNUM[num].style.color = "#ff3300";
@@ -94,16 +104,67 @@ document.addEventListener("DOMContentLoaded", function () {
     cardMid[num].innerHTML = cards[n].suit;
     cardBot[num].innerHTML = cards[n].value;
 
-    if (num == 0) {
+    scoreSum(n, num, flag);
+  }
+
+  function scoreSum(n, num, flag) {
+    if (num == 5 && flag === true) {
       score.innerHTML = cards[n].value;
-    } else {
+    } else if (flag === true) {
       score.innerHTML += "+" + cards[n].value;
     }
   }
 
+  function summaCards(vals) {
+    let summa = 0;
+    let counter = 0;
+    // Счет игрока
+    vals.forEach((elem) => {
+      if (!isNaN(parseFloat(Number(elem))) && isFinite(Number(elem))) {
+        summa += Number(elem);
+        if (summa > 21 && counter > 0) {
+          summa -= counter * 10;
+          counter--;
+          console.log("+11 -10: +1");
+        }
+      } else if (elem == "K" || elem == "Q" || elem == "J") {
+        summa += 10;
+        if (summa > 21 && counter > 0) {
+          summa -= counter * 10;
+          console.log("+11 -10: +1");
+        }
+      } else if (elem == "A") {
+        counter++;
+        summa += 11;
+        if (summa > 21) {
+          summa -= counter * 10;
+          counter--;
+          console.log("+11 -10: +1");
+        }
+      }
+      console.log("itog:", summa);
+    });
+    return summa;
+  }
+
+  let arrSum = [];
   startBtn.addEventListener("click", () => {
-    getCard(num);
-    num++;
+    if (num < 10) {
+      getCard(num, true);
+      arrSum.push(cardBot[num].innerHTML);
+      num++;
+      console.log(summaCards(arrSum));
+    } else {
+      alert("У вас максимум карт");
+    }
+  });
+
+  stopBtn.addEventListener("click", () => {
+    if (num > 6 && numD < 4) {
+      numD++;
+      getCardDealer(numD);
+    }
+    // console.log(summaCards("Дилер", cardNUM.slice(0, 4)));
   });
 
   //
