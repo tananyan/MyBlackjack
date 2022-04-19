d4841506faab0197c57895f3be056cefe7401d88
Boris <Boris242008@ya.ru>
2022-04-19 23:53:16 +0300

    vh

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,6 +2,11 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
+  //
+  let vh = window.innerHeight * 0.01;
+  // Then we set the value in the --vh custom property to the root of the document
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+  //
   const suits = ["♠", "♣", "♥", "♦"];
   const values = [
     "A",
